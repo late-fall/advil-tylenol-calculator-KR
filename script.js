565228619c77f3c9f91efe6cadac8aa729798279
late-fall <shanwnsolee@gmail.com>
2022-11-18 01:16:04 -0500

    weight type input, img specific

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const advil = document.querySelector('.advil')
-const tylenol = document.querySelector('.tylenol')
-const wtAdvil = document.querySelector('#advil-wt')
-const wtTylenol = document.querySelector('#tylenol-wt')
+const advil = document.querySelector('.advil');
+const tylenol = document.querySelector('.tylenol');
+let weight = document.querySelector("#weight");
 
 // eventlistener when age is selected.
 document.querySelectorAll('input[name="age"]').forEach((elem)=>{
@@ -18,12 +17,11 @@ document.querySelectorAll('input[name="age"]').forEach((elem)=>{
 function selectAge() {
     let age = document.querySelector('input[name="age"]:checked').value
     document.querySelector('button').style.display = "none";
-    document.querySelector('.wt-title').style.display = "none";    
-    document.querySelectorAll('select').forEach((e) => {
-        e.selectedIndex = 0;
-        e.style.display = "none";
-    })
-    document.querySelector('img').style.display = "none";
+    document.querySelector('.wt-title').style.display = "none";   
+    document.querySelectorAll('.img').forEach((e) => {e.style.display = "none";});
+    document.querySelector('label[for="weight"]').style.display = "none";
+    document.querySelector('#weight').value = '';
+    document.querySelectorAll('input[name="med"').forEach((e) => {e.checked = false;})
     if (age == "under3"){
         document.querySelector('#result').innerHTML = "3개월 이하 아기가 열이 있다면 응급실로 데려가세요.";
         
@@ -44,8 +42,6 @@ function selectAge() {
 document.querySelectorAll('input[name="med"]').forEach((elem)=>{
     elem.addEventListener('change',function(){
         document.querySelector('.wt-title').style.display = "none";
-        wtTylenol.style.display = "none";
-        wtAdvil.style.display = "none";
         document.querySelector('#result').innerHTML = "";
         selectMed();
     });
@@ -55,106 +51,126 @@ document.querySelectorAll('input[name="med"]').forEach((elem)=>{
 function selectMed() {
     document.querySelector('.wt-title').style.display = "initial";
     let med = document.querySelector('input[name="med"]:checked').value
-    document.querySelector('img').style.display = "none";
+    document.querySelectorAll('.img').forEach((e) => {e.style.display = "none";});
     document.querySelector('button').style.display = "none";
-    document.querySelectorAll('select').forEach((e) => {
-        e.selectedIndex = 0;
-        e.style.display = "none";
-    })
-    if (med == "tylenol"){
-        wtTylenol.style.display = "initial";
-    }
-    else {
-        wtAdvil.style.display = "initial";
-    }
+    document.querySelector('label[for="weight"]').style.display = "initial";
+    document.querySelector('#weight').value = '';
 }
 
+//weight input
+weight.addEventListener('input', calc)
 
-// eventlistner for tylenol wt selection
-document.querySelector('#tylenol-wt').addEventListener('change',function(){
+// function to calculate input and show result
+function calc(){
     document.querySelector('#result').innerHTML = "";
     document.querySelector('button').style.display = "initial";
-    document.querySelector('img').style.display = "block";
-    calcTylenol();
-});
+    document.querySelectorAll('.img').forEach((e) => {e.style.display = "none";});
+    let medType = document.querySelector('input[name="med"]:checked').value;
+    let wt = weight.value;
+    let mg, tablet = "";
 
-// function for tylenol calculation.
-function calcTylenol() {
-    let wt = document.querySelector('#tylenol-wt').value
-    let mg = ""
-    let tablet = ""
-    if (wt == "11"){
-        mg = "162.5"
-        tablet = "반알(1/2)";
+    if (medType == "tylenol"){
+        if (wt >= 5 && wt <11){
+            mg = "81.25";
+            tablet = "1/4알"
+        }
+        else if (wt <= 11){
+            mg = "162.5";
+            tablet = "반알(1/2)";
+        }
+        else if (wt <= 16){
+            mg = "243.75";
+            tablet = "3/4알";
+        }
+        else if (wt <= 22){
+            mg = "325";
+            tablet = "한알";
+        }
+        else if (wt <= 33){
+            mg = "487.5";
+            tablet = "한알 반";
+        }
+        else {
+            mg = "650";
+            tablet = "두알";
+        }
+        
+        if (wt < 5){
+            document.querySelector('#result').innerHTML = "체중 5KG 이하인 경우 먼저 의사와 상담하세요."
+        }
+        else {
+            document.querySelector('#result').innerHTML = "일반 타이레놀 " + tablet + 
+            " tablet (" + mg + "mg) " + "을 4시간마다 주세요(하루 최대 5알)."
+        }
     }
-    else if (wt == "16"){
-        mg = "243.75"
-        tablet = "3/4알"
+
+    else if (medType == "advil"){
+        if (wt >= 5 && wt <10){
+            mg = "50";
+            tablet = "1/4알"
+        }
+        else if (wt <= 10){
+            mg = "100"
+            tablet = "반알(1/2)";
+        }
+        else if (wt <= 20){
+            mg = "200"
+            tablet = "한알"
+        }
+        else if (wt <= 30){
+            mg = "300"
+            tablet = "한알 반"
+        }
+        else {
+            mg = "400"
+            tablet = "두알"
+        }
+        if (wt < 5){
+            document.querySelector('#result').innerHTML = "체중 5KG 이하인 경우 먼저 의사와 상담하세요."
+        }
+        else {
+            document.querySelector('#result').innerHTML = "일반 애드빌 " + tablet + 
+            " tablet (" + mg + "mg) " + "을 6시간마다 주세요."
+        }
     }
-    else if (wt == "22"){
-        mg = "325"
-        tablet = "한알"
+
+    else{
+        console.log("error calculating dose")
     }
-    else if (wt == "33"){
-        mg = "487.5"
-        tablet = "한알 반"
+
+    if (tablet == "1/4알"){
+        document.querySelector('.img-qtr').style.display = "block";
+    }
+    else if (tablet == "반알(1/2)"){
+        document.querySelector('.img-half').style.display = "block";
+    }
+    else if (tablet == "3/4알"){
+        document.querySelector('.img-threeqtr').style.display = "block";
+    }
+    else if (tablet == "한알"){
+        document.querySelector('.img-one').style.display = "block";
+    }
+    else if (tablet == "한알 반"){
+        document.querySelector('.img-onehalf').style.display = "block";
+    }
+    else if (tablet == "두알"){
+        document.querySelector('.img-two').style.display = "block";
     }
     else {
-        mg = "650"
-        tablet = "두알"
+        console.log("error showing image");
     }
-    document.querySelector('#result').innerHTML = "타이레놀 " + tablet + 
-    " tablet (" + mg + "mg) " + "을 4시간마다 주세요(하루 최대 5알)."
 }
-
-
-
-// eventlistner for advil wt selection
-document.querySelector('#advil-wt').addEventListener('change',function(){
-    document.querySelector('#result').innerHTML = "";
-    document.querySelector('button').style.display = "initial";
-    document.querySelector('img').style.display = "block";
-    calcAdvil();
-});
-
-// function for advil calculation.
-function calcAdvil() {
-    let wt = document.querySelector('#advil-wt').value
-    let mg = ""
-    let tablet = ""
-    if (wt == "10"){
-        mg = "100"
-        tablet = "반알(1/2)";
-    }
-    else if (wt == "20"){
-        mg = "200"
-        tablet = "한알"
-    }
-    else if (wt == "30"){
-        mg = "300"
-        tablet = "한알 반"
-    }
-    else {
-        mg = "400"
-        tablet = "두알"
-    }
-    document.querySelector('#result').innerHTML = "애드빌 " + tablet + 
-    " tablet (" + mg + "mg) " + "을 6시간마다 주세요."
-}
-
 
 // event listner for reset button
 document.querySelector("button").addEventListener('click', function(){
     document.querySelector('#result').innerHTML = "";
     document.querySelector('.wt-title').style.display = "none";
     document.querySelector('.med-title').style.display = "none";
-    document.querySelectorAll('select').forEach((e) => {
-        e.selectedIndex = 0;
-        e.style.display = "none";
-    })
-    document.querySelectorAll('input').forEach((e) => {e.checked = false;})
+    document.querySelectorAll('input').forEach((e) => {e.checked = false;});
     document.querySelector('button').style.display = "none";
-    document.querySelector('img').style.display = "none";
+    document.querySelectorAll('.img').forEach((e) => {e.style.display = "none";});
+    document.querySelector('label[for="weight"]').style.display = "none";
+    document.querySelector('#weight').value = '';
     tylenol.style.display = "none";
     advil.style.display = "none";
 })
